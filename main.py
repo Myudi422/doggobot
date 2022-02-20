eba69cd94826fd91d64761dd280507fd233ddfc6
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-20 17:32:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_image_url():
     file_extension = ''
     while file_extension not in allowed_extension:
         url = get_url()
-        file_extension = re.search("([^.]*)$",result).group(1).lower()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
     return url
 def bop(bot, update):
     url = get_image_url()
