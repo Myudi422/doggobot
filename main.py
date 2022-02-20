f17744b123f3f39cf67b79cd9cc9de23a9198342
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-20 17:30:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ def get_image_url():
     file_extension = ''
     while file_extension not in allowed_extension:
         url = get_url()
-        file_extension = re.search("([^.]*)$",url).group(1).lower()
+        file_extension = re.search("([^.]*)$",result).group(1).lower()
     return url
 def bop(bot, update):
     url = get_image_url()
     chat_id = update.message.chat_id
-    bot.send_photo(chat_id=chat_id, photo=url)
+    bot.send_photo(chat_id=chat_id, photo=result)
 def main():
     updater = Updater('1811863530:AAFFYY_EqvKapAYf93HiO2j3qD-pGKNHgLg')
     dp = updater.dispatcher
