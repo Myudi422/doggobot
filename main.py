6f4242e9a48442d0b2b4ebbdd62de110fb36ad1f
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-20 16:54:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-from telegram.ext.dispatcher import run_async
 import requests
 import re
-
 def get_url():
     contents = requests.get('https://arugaz.herokuapp.com/api/nekonime').json()
     url = contents['url']
     return url
-
 def get_image_url():
     allowed_extension = ['jpg','jpeg','png']
     file_extension = ''
@@ -15,19 +12,15 @@ def get_image_url():
         url = get_url()
         file_extension = re.search("([^.]*)$",url).group(1).lower()
     return url
-
-run_async
-def bop(update, context):
+def bop(bot, update):
     url = get_image_url()
     chat_id = update.message.chat_id
-    context.bot.send_photo(chat_id=chat_id, photo=url)
-
+    bot.send_photo(chat_id=chat_id, photo=url)
 def main():
-    updater = Updater('1811863530:AAFFYY_EqvKapAYf93HiO2j3qD-pGKNHgLg', use_context=True)
+    updater = Updater('1811863530:AAFFYY_EqvKapAYf93HiO2j3qD-pGKNHgLg')
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('bop',bop))
     updater.start_polling()
     updater.idle()
-
 if __name__ == '__main__':
     main()
