4ebe578d257c17f4994c42cdbfa5f82f74d317b2
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-02-20 17:59:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def get_image_url():
 
 @run_async
 def bop(update, context):
+    msg = update.effective_message
     url = get_image_url()
-    chat_id = update.message.chat_id
     context.bot.send_photo(chat_id=chat_id, photo=url)
     keyboard = [[InlineKeyboardButton(text="Send as file", callback_data=f"neko_callback, {link}, neko"),InlineKeyboardButton(text=f"Direct link",url=f"https://cdn.nekos.life/{link[0]}")]]
     keyboard += [[InlineKeyboardButton(text=delete_button, callback_data=f"neko_delete, {msg.from_user.id}")]]
